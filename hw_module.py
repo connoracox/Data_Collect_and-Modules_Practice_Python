a228a55397c5773ff343cad20d5f5398381b62e9
Connor Cox <connor.a.cox@gmail.com>
2023-02-02 08:43:01 -0500

    first commit

---
+++ hw_module.py
@@ -0,0 +1,10 @@
+def square_footage(length, width):
+    area = length * width 
+    return f"The square footage of your house is: {area} sqft."
+
+from math import pi
+
+def circ_circ(radius):
+    circumference = 2 * pi * radius
+    return f" The circumference of your circle is: {round(circumference, 3)} ft."
+
